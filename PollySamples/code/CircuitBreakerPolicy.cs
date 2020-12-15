0cc577ed1cb49f20ab51f5e9a41aa0b54358fdd5
Anish Aravind <anish.arvind407@gmail.com>
2020-12-15 22:54:24 +0530

    CIRCUIT BREAKER ADVANCED POLICY

--- PollySamples/code/CircuitBreakerPolicy.cs
+++ PollySamples/code/CircuitBreakerPolicy.cs
@@ -10,6 +10,7 @@ namespace PollySamples.code
     public class CircuitBreakerPoliciesWrapper
     {
         public CircuitBreakerPolicy<HttpResponseMessage> BreakerPolicy { get; set; }
+        public CircuitBreakerPolicy<HttpResponseMessage> AdvancedBreakerPolicy { get; set; }
         public RetryPolicy<HttpResponseMessage> RetryPolicy { get; set; }
 
         public CircuitBreakerPoliciesWrapper()
@@ -17,7 +18,14 @@ namespace PollySamples.code
             BreakerPolicy = Policy.HandleResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
                              .CircuitBreakerAsync(2, TimeSpan.FromSeconds(5));
 
-            RetryPolicy=Policy.HandleResult<HttpResponseMessage>(res=> !res.IsSuccessStatusCode)
+            AdvancedBreakerPolicy = Policy.HandleResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
+                             //1-> 0.5 = IF 50% of requests are  !response.IsSuccessStatusCode
+                             //2-> TimeSpan.FromSeconds(60) = IF 50% OF REQ WITHIN 60 SEC
+                             //3-> 7 -- IF MAX NO OF REQ WITHIN 60 SECS IS LESS THAN 7 THEN IGNORE POLICY
+                             //4-> TimeSpan.FromSeconds(3) -- OPEN CIRCUIT FOR 3 SECS
+                             .AdvancedCircuitBreakerAsync(0.5,TimeSpan.FromSeconds(60),7,TimeSpan.FromSeconds(3));
+
+            RetryPolicy =Policy.HandleResult<HttpResponseMessage>(res=> !res.IsSuccessStatusCode)
                        .Or<BrokenCircuitException>()
                        .RetryAsync(25, async (response, retryCount) =>
                        {
